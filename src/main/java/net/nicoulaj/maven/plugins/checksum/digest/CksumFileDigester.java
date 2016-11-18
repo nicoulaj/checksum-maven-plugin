f2cdc45cee084096e7af061043c7468fe3833a2a
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2016-11-18 23:35:47 +0100

    Re-generate license headers

--- src/main/java/net/nicoulaj/maven/plugins/checksum/digest/CksumFileDigester.java
+++ src/main/java/net/nicoulaj/maven/plugins/checksum/digest/CksumFileDigester.java
@@ -1,10 +1,25 @@
+/*-
+ * ====================================================================
+ * checksum-maven-plugin
+ * ====================================================================
+ * Copyright (C) 2010 - 2016 Julien Nicoulaud <julien.nicoulaud@gmail.com>
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
 package net.nicoulaj.maven.plugins.checksum.digest;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Implementation of {@link FileDigester} for the POSIX 1003.2 CRC algorithm (32 bits).
